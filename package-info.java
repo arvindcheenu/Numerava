574d6c5c9b05ff4834b8a77c83f8cf066ef76203
Arvind Srinivasan <arvind@cheenu.net>
2017-12-18 14:44:55 +0530

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -10,12 +10,7 @@
  */
 package humanize;
 /*
- * @Numeral.class : This class is used for generating the cardinal, ordinal and nominal
+ * @Numerava.class : This class is used for generating the cardinal, ordinal and nominal
  * 					representations of the input integer. 
  * 					For Output tests check writeNumerals.java
- * 
- * 
- * @changeCase.class : This class is used for generating different types of string cases
- * 					   from input string. 
- * 					   For Output tests check StringCaseChange.java
  */
